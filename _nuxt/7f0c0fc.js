df626031dc112e38c2362c2dcce21e511ff89796
Batyr Akmyradov <akmrbat@gmail.com>
2022-06-20 12:40:05 +0500

    Inital commit

---
+++ _nuxt/7f0c0fc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{416:function(e){e.exports=JSON.parse('{"example":"Naprimer"}')}}]);
---
+++ _nuxt/static/1655710684/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fru\u002Flibrary","\u002Flibrary","\u002Fnews","\u002Fmedia","\u002Fru\u002Fnews","\u002Fpolitics","\u002Fru\u002Fmedia","\u002Fru\u002Fpolitics","\u002Fru"]})
