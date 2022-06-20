f93cbd2f9914d74ebf9d82a38e65fe9e48a5a2bb
Batyr Akmyradov <akmrbat@gmail.com>
2022-06-20 21:18:35 +0500

    feat: footer address changed

---
+++ _nuxt/static/1655741872/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fsections","\u002Fru\u002Flibrary","\u002Fnews","\u002Fru\u002Fnews","\u002Fmedia","\u002Fru\u002Fsections","\u002Fru","\u002Flibrary","\u002Fru\u002Fmedia"]})
