67187a07ee278f8f8af2aa0e7421037fbc073e7c
Batyr Akmyradov <akmrbat@gmail.com>
2022-06-22 12:06:31 +0500

    feat: mobile and tablet headers fixed

---
+++ _nuxt/static/1655881207/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fru\u002Flibrary","\u002Fsections","\u002Fnews","\u002Fru\u002Fmedia","\u002Fru","\u002Flibrary","\u002Fru\u002Fsections","\u002Fmedia","\u002Fru\u002Fnews"]})
