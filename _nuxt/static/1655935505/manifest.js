5d98b2b5a4cb71a6a0232403e575707c5ca2dd5c
Batyr Akmyradov <akmrbat@gmail.com>
2022-06-23 03:05:54 +0500

    feat: major bug fixes translations added

---
+++ _nuxt/static/1655935505/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fmedia","\u002Fru\u002Fmedia","\u002Flibrary","\u002Fnews","\u002Fsections","\u002Fscrmet","\u002Fru","\u002Fru\u002Fsections","\u002Fru\u002Fnews","\u002Fru\u002Flibrary","\u002Fru\u002Fscrmet"]})
